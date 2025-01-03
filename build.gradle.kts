04dfac976483183f2aac365cff9d4aa338483ef4
진수 <moonlt93@naver.com>
2025-01-03 23:50:57 +0900

    build: kotest , ktlint 적용을 위한 gradle 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "3.3.7"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "1.9.25"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
 }
 
 fun getGitHash(): String {
@@ -51,6 +52,16 @@ dependencies {
     //swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
+
+    // Kotest 의존성
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+
+    // MockK 의존성
+    testImplementation("io.mockk:mockk:1.13.7")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
@@ -76,6 +87,16 @@ tasks.withType<Test> {
     useJUnitPlatform()
     systemProperty("user.timezone", "UTC")
 }
+
+val installLocalGitHook =
+    tasks.register<Copy>("installLocalGitHook") {
+        from("${rootProject.rootDir}/hooks")
+        into(File("${rootProject.rootDir}/.git/hooks"))
+
+        eachFile {
+            mode = "755".toInt(radix = 8)
+        }
+    }
 
 dependencyManagement {
     imports {
